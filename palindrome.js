46205f8a3b5491da964229062e789f2f0868ad21
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-02-04 07:38:18 -0600

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -2,8 +2,10 @@ function palindrome(){
 var new1 = "";
 var g = prompt(" votre charactere :");
 let size = g.length;
- if(size === 1)
+ if(size === 1){
     alert("Palindrome de taille 1");
+    return g;
+ }
  else{
       for(var i = size - 1 ; i >= 0; i--){
         new1 = new1 + g[i];
