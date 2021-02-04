4db4c45fafa526f542a8490a10fd34c4bacfefbe
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-02-04 07:35:11 -0600

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+function palindrome(){
+var new1 = "";
+var g = prompt(" votre charactere :");
+let size = g.length;
+ if(size === 1)
+    alert("Palindrome de taille 1");
+ else{
+      for(var i = size - 1 ; i >= 0; i--){
+        new1 = new1 + g[i];
+   }
+       if(new1 === g)
+          alert("palindrome");
+       else
+          alert("Not Palindrome");
+ }
+return new1;
+}
+palindrome();
